1e7cd5ab1b3e77e55b7981f34e6ef566de68a38e
hopolivier <open.lelong@laposte.net>
2016-05-12 12:13:10 +0200

    model validation before_destroy

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,13 @@
 class Ingredient < ActiveRecord::Base
   has_many :doses
   validates :name, presence: true, uniqueness: true
+  before_destroy :safe_destroy
+
+  private
+
+  def safe_destroy
+    return true if self.doses.count == 0
+    raise 'Cannot destroy ingredient - remaining doses'
+  end
+
 end
