555f0e0590066f0184db7908bd026ae3be735df2
hopolivier <open.lelong@laposte.net>
2016-05-13 16:03:29 +0200

    added two-size images

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,4 +2,17 @@
 
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+  process eager: true  # Force version generation at upload time.
+
+  process convert: 'jpg'
+
+  version :standard do
+    cloudinary_transformation height: 300, width: 400, crop: :fill
+    # resize_to_fit 800, 600
+  end
+
+  version :thumb do
+    cloudinary_transformation height: 90, width: 90, crop: :thumb, gravity: :face, radius: 20
+  end
+
 end
